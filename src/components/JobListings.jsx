c3faca59e59d82025bb9036bb18c40630356a48b
midnightstarlight <malabaopie@gmail.com>
2024-03-20 22:08:13 +0800

    add viewalljobs section

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -4,7 +4,9 @@ import jobs from '../jobs.json';
 
 const JobListings = () => {
 
-    console.log(jobs);
+    // console.log(jobs);
+
+    const recentJobs = jobs.slice(0, 3);
 
     return (
         < section className="bg-blue-50 px-4 py-10" >
@@ -14,7 +16,7 @@ const JobListings = () => {
                 </h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-                    {jobs.map((job) => (
+                    {recentJobs.map((job) => (
                         <JobListing key={job.id} job={job} />
                     ))}
 
