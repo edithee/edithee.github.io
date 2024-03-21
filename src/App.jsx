1121f3cc77a32ac88c6a2a1719f8a63b0aaa8b4f
midnightstarlight <malabaopie@gmail.com>
2024-03-21 13:13:12 +0800

    setup react-router-dom and refactor to use

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
+// import Navbar from './components/Navbar';
+// import Hero from './components/Hero';
+// import HomeCards from './components/HomeCards';
+// import JobListings from './components/JobListings';
+// import ViewAllJobs from './components/ViewAllJobs';
+
+import HomePage from './pages/HomePage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route index element={<HomePage />} />)
+);
 
 const App = () => {
-  return (
-    // <div>App</div>
-    <>
-      <Navbar />
-      <Hero title='Test Title' subtitle='This is a subtitly' />
 
-      <HomeCards />
+  return <RouterProvider router={router} />;
 
-      <JobListings />
-
-      <ViewAllJobs />
-
-
-    </>
-  );
 };
 
 export default App;
 
+
+// return (
+//   // <div>App</div>
+//   <>
+//     <Navbar />
+//     <Hero title='Test Title' subtitle='This is a subtitly' />
+
+//     <HomeCards />
+
+//     <JobListings />
+
+//     <ViewAllJobs />
+
+
+//   </>
+// );
