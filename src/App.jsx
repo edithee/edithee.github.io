852bdacdd220a6d49d73576428441c9ae5518f73
midnightstarlight <malabaopie@gmail.com>
2024-03-21 22:16:12 +0800

    add useEffect to get singular job from proxy w dynamic element id

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import E404Page from './pages/E404Page';
+import JobPage from './pages/JobPage';
 
 
 const router = createBrowserRouter(
@@ -18,6 +19,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} />
       <Route path='*' element={<E404Page />} />
     </Route>
   )
