1112195648c357decfb42288bb1d745075eff733
midnightstarlight <malabaopie@gmail.com>
2024-03-21 22:26:30 +0800

    REF: add react date loaders to load data, jobLoader can be used by other components

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
 import E404Page from './pages/E404Page';
-import JobPage from './pages/JobPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 
 const router = createBrowserRouter(
@@ -19,7 +19,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
-      <Route path='/jobs/:id' element={<JobPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<E404Page />} />
     </Route>
   )
