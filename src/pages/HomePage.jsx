d45b4efd29f01fbfbf794457686e32b16c87e9bc
midnightstarlight <malabaopie@gmail.com>
2024-03-21 13:21:03 +0800

    REF: Homepage to use created elements

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,16 @@
 import Hero from "../components/Hero";
-
+import HomeCards from "../components/HomeCards";
+import JobListings from "../components/JobListings";
+import ViewAllJobs from "../components/ViewAllJobs";
 
 const HomePage = () => {
     return (
-        <Hero title='Test Title' subtitle='This is a subtitly' />
+        <>
+            <Hero title='Test Title' subtitle='This is a subtitly' />
+            <HomeCards />
+            <JobListings />
+            <ViewAllJobs />
+        </>
     );
 };
 
